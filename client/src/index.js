3fb1ffc1cc4d9cb6ed7787fc5ffa9764825d34e6
awab24 <awab1hassan1@gmail.com>
2025-04-12 19:01:05 +0300

    adding everything

--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,46 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import { PersistGate } from 'redux-persist/integration/react';
+import { allowReducer } from './reducers/reducers';
+
+// 1. Set up persist configuration
+const persistConfig = {
+  key: 'root', // You can give any key
+  storage,     // Use local storage
+};
+
+// 2. Create a persisted reducer
+const persistedReducer = persistReducer(persistConfig, allowReducer);
+
+// 3. Configure the store with the persisted reducer
+const store = configureStore({
+  reducer: {
+    allow: persistedReducer, // Persist the 'allow' reducer
+  },
+});
+
+// 4. Create the persistor
+const persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        {/* 5. Add PersistGate to delay rendering until rehydration is complete */}
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
